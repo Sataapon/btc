355b610f8a13fd49a9807dc37fcd67c364ccf705
sataapon <mr.sataapon@gmail.com>
2022-03-08 23:34:46 +0700

    scripts: added script for setup env

---
+++ scripts/env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export Hostname=localhost
+export ServicePort=8080
+
+export DriverName=postgres
+
+export PostgreSqLServer=localhost
+export PostgreSqlPort=5432
+export PostgreSqlUser=postgres
+export PostgreSqlPassword=123456789
+export PostgreSqlDatabase=wallet 
